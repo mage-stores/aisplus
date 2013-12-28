f9a3b8d02b1d01d33d930a191dc71ccda2642f07
dmitry <iekutenko@gmail.com>
2013-12-28 19:36:59 +0200

    first iwd_all

---
+++ app/code/community/IWD/All/Model/Observer.php
@@ -0,0 +1,18 @@
+<?php
+class IWD_All_Model_Observer
+{
+
+    public function preDispatch(Varien_Event_Observer $observer)
+    {
+
+        if (Mage::getSingleton('admin/session')->isLoggedIn()) {
+
+            $feedModel  = Mage::getModel('iwdall/feed');
+            /* @var $feedModel Mage_AdminNotification_Model_Feed */
+
+            $feedModel->checkUpdate();
+
+        }
+
+    }
+}
